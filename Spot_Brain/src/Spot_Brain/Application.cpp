b81c6f4f53bbbd32550cfb1e4f4d2580d84f0b81
bad-programmer800 <venusrising800@gmail.com>
2022-11-19 13:21:08 -0500

    Added window event using Cherno Game Engine ep. 12

--- Spot_Brain/src/Spot_Brain/Application.cpp
+++ Spot_Brain/src/Spot_Brain/Application.cpp
@@ -1,17 +1,29 @@
 #include "sbpch.h"
+
 #include "Application.h"
-#include "Spot_Brain/Events/ApplicationEvent.h"
+
 #include <GLFW/glfw3.h>
 
 namespace Brainspace {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() 
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() 
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SB_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run() 
@@ -22,6 +34,12 @@ namespace Brainspace {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- Spot_Brain/src/Spot_Brain/Application.h
+++ Spot_Brain/src/Spot_Brain/Application.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "Core.h"
+
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Brainspace {
@@ -14,7 +17,12 @@ namespace Brainspace {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 
